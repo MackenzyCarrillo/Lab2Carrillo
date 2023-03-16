f56ea75a3d504bd5e2c3f25690b8b8cf50459db2
MackenzyCarrillo <709666@student.dusd.net>
2023-03-16 15:27:32 -0700

    iuii
    iii

---
+++ Lab2Carrillo/Assets/Scripts/PlayerControllers.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllers : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+}
